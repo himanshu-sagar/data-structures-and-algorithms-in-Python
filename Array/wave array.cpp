9f23c6b5fde3acb643d5b16335874afdc21744e1
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-05 13:59:23 +0530

    Delete wave array.cpp

--- Array/wave array.cpp
+++
@@ -1,9 +0,0 @@
-void convertToWave(int *arr, int n){
-    for(int i=0;i<n;i+=2) { //only even places
-        if(i<n-1 && arr[i] < arr[i+1])
-            swap(arr[i], arr[i+1]);
-            
-        if(i>0 && arr[i] < arr[i-1])
-            swap(arr[i], arr[i-1]);
-    }
-}
