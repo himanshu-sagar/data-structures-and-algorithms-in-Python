bdcd57e94792d1e41a4fb93ba86de91d9834c051
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-05 13:11:38 +0530

    Create leaders in an array.py

---
+++ Array/leaders in an array.py
@@ -0,0 +1,12 @@
+def leader(arr,n):
+    l=arr[n-1]
+    leader=[]
+    leader.append(l)
+    for i in range(n-2,-1,-1):
+        if l<=arr[i]:
+            l=arr[i]
+            leader.append(l)
+    leader.reverse()
+    return leader
+arr=[7,4,5,7,3]
+leader(arr,len(arr))
