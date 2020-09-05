415d8883dbb00986ea4c5c6820363485e4f681e1
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-05 12:51:41 +0530

    Update second max.py

--- Array/second max.py
+++ Array/second max.py
@@ -1,5 +1,12 @@
-def secondMax(arr,n):
+def secondLargest(arr,n):
+    arr=set(arr)
+    arr=list(arr)
     arr.sort()
-    return arr[-2]
-arr=[1,4,3,3,4,5,3,1,2,2]
-secondMax(arr,len(arr))
+    n=len(arr)
+    if len(arr)==1:
+        return arr[n-1],-1
+    else:
+        return arr[n-1],arr[n-2]
+    
+arr=[1,1,1,1,67,1,6,8]
+secondLargest(arr,len(arr))
