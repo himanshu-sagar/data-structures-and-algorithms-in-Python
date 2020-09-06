b2bc928aa109178ed464e88596e58c74b53983d3
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-06 10:09:24 +0530

    Update is height balanced.py

--- Tree/is height balanced.py
+++ Tree/is height balanced.py
@@ -6,3 +6,20 @@ def isHeightbalanced(tree):#O(n^2)
     x=lh-rh
     return (abs(x)<=1 and isHeightbalanced(tree.left) and isHeightbalanced(tree.right))
     
+def isHeightbalanced1(tree):#O(n)
+    if tree==None:
+        return 0
+    lh=isHeightbalanced1(tree.left)
+    if lh==-1:
+        return -1
+    rh=isHeightbalanced1(tree.right)
+    if rh==-1:
+        return -1
+    x=abs(lh-rh)
+    if x>1:
+        return -1
+    else:
+        return max(lh,rh)+1
+    
+    
+        
