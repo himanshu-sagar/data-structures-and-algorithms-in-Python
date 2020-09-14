4e6fcf861826b9babe4efbfbc15146d54a02723b
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-14 13:31:40 +0530

    Create balanced(Brackett) expression.py

---
+++ Stacks/balanced(Brackett) expression.py
@@ -0,0 +1,25 @@
+def matching(c1,c2):
+    if (c1=='(') & (c2==')'):
+        return True
+    elif (c1=='{') & (c2=='}'):
+        return True
+    elif (c1=='[') & (c2==']'):
+        return True
+    else:
+        return False
+def isBalanced(s):
+    from queue import LifoQueue 
+    st = LifoQueue() 
+    for i in range(len(s)):
+        if ((s[i] == '{') | (s[i] == '[') | (s[i] == '(')):
+            st.put(s[i])
+        else:
+            if st.empty()==True:
+                return False
+            elif matching(st.get(),s[i])==False:
+                return False
+    if st.empty()==True:
+        return True
+    else:
+        return False
+print(isBalanced("[{()}(])"))         
