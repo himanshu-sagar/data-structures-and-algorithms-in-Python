869b7a77d271d75d8a84441707a829d3647f58f9
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-14 13:35:55 +0530

    Create next smaller problem.py

---
+++ Stacks/next smaller problem.py
@@ -0,0 +1,17 @@
+def nextsmaller(arr,n):
+    st2=[]
+    s=[]
+    st2.append(arr[-1])
+    s.append(-1)
+    for i in range(n-2,-1,-1):
+        while (len(st2)>0 and st2[-1]>=arr[i]):
+            st2.pop()
+        if len(st2)<=0:
+            s.append(-1)
+        else:
+            s.append(st2[-1])
+        st2.append(arr[i])
+    s.reverse()
+    return s
+arr=[5,15,10,8,6,12,9,18]
+print(nextsmaller(arr,len(arr)))
