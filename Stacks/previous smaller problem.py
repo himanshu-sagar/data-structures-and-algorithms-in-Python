9e0bd4f061cf78172c48072038ed02f3e940cf2a
Himanshu sagar <49094337+sagar-ML-Developer@users.noreply.github.com>
2020-09-14 13:35:18 +0530

    Create previous smaller problem.py

---
+++ Stacks/previous smaller problem.py
@@ -0,0 +1,16 @@
+def prevsmaller(arr):
+    st2=[]
+    st2.append(arr[0])
+    s=[]
+    s.append(-1)
+    for i in range(1,len(arr)):
+        while (len(st2)>0 and st2[-1]>=arr[i]):
+            st2.pop()
+        if len(st2)<=0:
+            s.append(-1)
+        else:
+            s.append(st2[-1])
+        st2.append(arr[i])
+    return s
+arr=[15,13,12,14,16,8,6,4,10,30]
+print(prevsmaller(arr))
