18d1f239e70f2776fcec49cd81c4a62b337e1427
himanshu-sagar <sagarhimanshu355@gmail.com>
2022-09-04 07:25:26 +0530

    remove secret key

--- linked_list/LinkedList.py
+++ linked_list/LinkedList.py
@@ -1,4 +1,19 @@
 class Node:
-    def __init__(self, data = None):
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def create_linked_list(self):
+        head = LinkedList()
+        data = int(input("Enter Elements(-1 to stop): "))
+        while (data != -1):
+            if head == None:
+                head = Node(data=data)
+                tail = Node(data=data)
+            else:
+                
